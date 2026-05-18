fa84c090c5092725a4916f9a3fef9d679d838969
karmawolf-git <karmawolf@gmail.com>
2026-05-18 18:20:07 +0900

    feat: wrap app with MsalProvider for Outlook authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './config/msalConfig';
+import './index.css';
+import App from './App.tsx';
+
+const pca = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MsalProvider instance={pca}>
+      <App />
+    </MsalProvider>
   </StrictMode>,
-)
+);
